20decf115508d5fa97ac4a85867c2ffb4ff32083
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-10-06 12:55:22 -0600

    Update centro_formacion.sql

--- project/centro_formacion.sql
+++ project/centro_formacion.sql
@@ -166,3 +166,82 @@ CREATE OR REPLACE TYPE BODY Asignatura AS
   END;
 END;
 /
+
+-- tabla para Alumnos
+CREATE TABLE AlumnosTable OF Alumnos (
+  PRIMARY KEY (DNI_P)
+);
+
+-- tabla para Profesores
+CREATE TABLE ProfesoresTable OF Profesores (
+  PRIMARY KEY (DNI_P)
+);
+
+-- tabla para Asignatura
+CREATE TABLE AsignaturaTable OF Asignatura (
+  PRIMARY KEY (Codigo_As)
+);
+
+CREATE TABLE Rel_Alumno_asignatura (
+  ID_Rel NUMBER PRIMARY KEY,
+  Alumno REF Alumnos,
+  Asignatura REF Asignatura,
+  Profesor REF Profesores
+);
+
+-- Insertar información en la tabla AlumnosTable
+INSERT INTO AlumnosTable (DNI_P, Nombre_P, Edad_A, DNI_A, Codigo_As)
+VALUES ('12345A', 'Erik Tamayo', 20, 'A1234', 'A001');
+
+-- Insertar información en la tabla ProfesoresTable
+INSERT INTO ProfesoresTable (DNI_P, Nombre_P, NCuenta_Pr, DNI_Pr, Codigo_As)
+VALUES ('P001', 'Profesor Matemáticas', 'M1234', 'P1234', 'A001');
+
+-- Insertar información en la tabla AsignaturaTable
+INSERT INTO AsignaturaTable (Codigo_As, Nombre_As, Nombre_Pr)
+VALUES ('A001', 'Matemáticas', 'Profesor Matemáticas');
+
+-- tabla de relación para el alumno "Erik Tamayo"
+INSERT INTO Rel_Alumno_asignatura (ID_Rel, Alumno, Asignatura, Profesor)
+VALUES (
+  1,
+  (SELECT REF(a) FROM AlumnosTable a WHERE a.DNI_P = '12345A'),
+  (SELECT REF(b) FROM AsignaturaTable b WHERE b.Codigo_As = 'A001'),
+  (SELECT REF(c) FROM ProfesoresTable c WHERE c.NCuenta_Pr = 'P001')
+);
+
+-- tabla AsignaturaTable para otras dos materias
+INSERT INTO AsignaturaTable (Codigo_As, Nombre_As, Nombre_Pr)
+VALUES ('A002', 'Historia', 'Profesor Historia');
+
+INSERT INTO AsignaturaTable (Codigo_As, Nombre_As, Nombre_Pr)
+VALUES ('A003', 'Ciencias', 'Profesor Ciencias');
+
+-- Insertar información en la tabla de relación para el mismo alumno con las nuevas asignaturas
+INSERT INTO Rel_Alumno_asignatura (ID_Rel, Alumno, Asignatura, Profesor)
+VALUES (
+  2,
+  (SELECT REF(a) FROM AlumnosTable a WHERE a.DNI_P = '12345A'),
+  (SELECT REF(b) FROM AsignaturaTable b WHERE b.Codigo_As = 'A002'),
+  (SELECT REF(c) FROM ProfesoresTable c WHERE c.NCuenta_Pr = 'P002')
+);
+
+INSERT INTO Rel_Alumno_asignatura (ID_Rel, Alumno, Asignatura, Profesor)
+VALUES (
+  3,
+  (SELECT REF(a) FROM AlumnosTable a WHERE a.DNI_P = '12345A'),
+  (SELECT REF(b) FROM AsignaturaTable b WHERE b.Codigo_As = 'A003'),
+  (SELECT REF(c) FROM ProfesoresTable c WHERE c.NCuenta_Pr = 'P003')
+);
+
+SELECT * FROM AlumnosTable;
+
+SELECT * FROM ProfesoresTable;
+
+SELECT * FROM AsignaturaTable;
+
+SELECT r.ID_Rel, a.Nombre_P AS Nombre_Alumno, b.Nombre_As AS Nombre_Asignatura, c.Nombre_P AS Nombre_Profesor
+FROM Rel_Alumno_asignatura r
+JOIN AlumnosTable a ON DEREF(r.Alumno) IS OF (Alumnos) AND DEREF(r.Alumno).DNI_A = 'A1234'
+JOIN AsignaturaTable b ON DEREF(r.Asignatura) IS OF (Asignatura)
+JOIN ProfesoresTable c ON DEREF(r.Profesor) IS OF (Profesores);
